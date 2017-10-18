cf54ba2d4c3580ff167279d41a497d603a9db318
soraj <buasri_soraj@hotmail.com>
2017-10-18 15:34:10 +0700

    echo test

--- botpush.php
+++ botpush.php
@@ -3,7 +3,7 @@
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('1541414817');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75c03f392f6e53d662d6f5a8db9e421f']);
 
-
+echo $bot;
 // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
 // $response = $bot->replyMessage('<reply token>', $textMessageBuilder);
 // if ($response->isSucceeded()) {
