b4fc013613afaa245d4099091ba772c1cb2fd05d
soraj <buasri_soraj@hotmail.com>
2017-10-19 10:49:41 +0700

    edit botpush

--- botpush.php
+++ botpush.php
@@ -11,18 +11,18 @@ $bot = new \LINE\LINEBot(
     ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
 );
 
-$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$body = file_get_contents("php://input");
+// $signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+// $body = file_get_contents("php://input");
 
-$events = $bot->parseEventRequest($body, $signature);
+// $events = $bot->parseEventRequest($body, $signature);
 
-foreach ($events as $event) {
-    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
-        $reply_token = $event->getReplyToken();
-        $text = $event->getText();
-        $bot->replyText($reply_token, $text);
-    }
-}
+// foreach ($events as $event) {
+//     if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+//         $reply_token = $event->getReplyToken();
+//         $text = $event->getText();
+//         $bot->replyText($reply_token, $text);
+//     }
+// }
 
-echo "OK";
+echo $bot ;
 
