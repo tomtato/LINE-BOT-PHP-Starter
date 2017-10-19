13f90fa04b01c0c8db8e3185f384c957df8c854c
soraj <buasri_soraj@hotmail.com>
2017-10-19 10:47:22 +0700

    no message

--- botpush.php
+++ botpush.php
@@ -1,6 +1,28 @@
 <?php
 
+define("LINE_MESSAGING_API_CHANNEL_SECRET", '75c03f392f6e53d662d6f5a8db9e421f
+Issue');
+define("LINE_MESSAGING_API_CHANNEL_TOKEN", '1541414817');
+
 require "vendor/autoload.php";
 
-echo "test";
+$bot = new \LINE\LINEBot(
+    new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
+    ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
+);
+
+$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$body = file_get_contents("php://input");
+
+$events = $bot->parseEventRequest($body, $signature);
+
+foreach ($events as $event) {
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+        $reply_token = $event->getReplyToken();
+        $text = $event->getText();
+        $bot->replyText($reply_token, $text);
+    }
+}
+
+echo "OK";
 
