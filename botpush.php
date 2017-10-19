57ed1182eebc24d8fda08401a35dbaedd625c4e5
soraj <buasri_soraj@hotmail.com>
2017-10-19 12:05:33 +0700

    edit

--- botpush.php
+++ botpush.php
@@ -6,7 +6,9 @@ require "vendor/autoload.php";
 
 $access_token = 'qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v2/bot/profile/Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+
+$url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
