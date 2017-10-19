cf990c5ecfa2ad77cc8c9d0500cb277a1da325a7
soraj <buasri_soraj@hotmail.com>
2017-10-19 11:21:03 +0700

    test

--- botpush.php
+++ botpush.php
@@ -7,13 +7,13 @@ require "vendor/autoload.php";
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/bot/group/1541414817/member/Uffa138efe037e6e889d0b0f4a871c005");
+curl_setopt($ch, CURLOPT_URL, "https://api.line.me/v2/bot/profile/1541414817");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
 
 $headers = array();
-$headers[] = "Authorization: Bearer 75c03f392f6e53d662d6f5a8db9e421f";
+$headers[] = "Authorization: Bearer Uffa138efe037e6e889d0b0f4a871c005";
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
