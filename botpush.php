885ca8bdf2c6cb8feef250db4482e8d4302b57bd
soraj <buasri_soraj@hotmail.com>
2017-10-19 11:55:54 +0700

    qweqwewqe

--- botpush.php
+++ botpush.php
@@ -6,7 +6,7 @@ require "vendor/autoload.php";
 
 $access_token = 'qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU=';
 
-$url = 'https://api.line.me/v2/bot/profile/1541414817';
+$url = 'https://api.line.me/v2/bot/profile/Uffa138efe037e6e889d0b0f4a871c005';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
