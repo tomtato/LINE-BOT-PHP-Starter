79a4c81a0d945314bbec9ce96bf7ebf86174d6fc
soraj <buasri_soraj@hotmail.com>
2017-10-19 14:48:17 +0700

    getMember

---
+++ getMember.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+require "vendor/autoload.php";
+
+$access_token = 'qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU=';
+
+$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->getRoomMemberProfile($channelSecret =, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+
+
+
+
+
+
+
