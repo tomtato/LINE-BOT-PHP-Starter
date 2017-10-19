9349c717e06e445da7a38b9149051c67717ea174
soraj <buasri_soraj@hotmail.com>
2017-10-19 11:24:57 +0700

    tewrew

--- botpush.php
+++ botpush.php
@@ -4,6 +4,7 @@
 
 require "vendor/autoload.php";
 
+$access_token = 'qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU=';
 
 $ch = curl_init();
 
@@ -12,8 +13,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
 
-$headers = array();
-$headers[] = "Authorization: Bearer u2e0cc06232508ddd44e6a79eb0e764bb";
+// $headers = array();
+// $headers[] = "Authorization: Bearer u2e0cc06232508ddd44e6a79eb0e764bb";
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
