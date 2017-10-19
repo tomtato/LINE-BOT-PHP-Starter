f0f33f7a91a3b82f502fa9e165512c117d025e3a
soraj <buasri_soraj@hotmail.com>
2017-10-19 13:53:14 +0700

    no message

--- botpush.php
+++ botpush.php
@@ -12,27 +12,13 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('<to>', $textMessageBuilder);
+$response = $bot->pushMessage('1541414817', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 
 
 
-// $access_token = 'qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU=';
 
-// $userId = 'Uffa138efe037e6e889d0b0f4a871c005';
 
-// $url = 'https://api.line.me/v2/bot/room/u2e0cc06232508ddd44e6a79eb0e764bb/member/'.$userId;
-
-// $headers = array('Authorization: Bearer ' . $access_token);
-
-// $ch = curl_init($url);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// $result = curl_exec($ch);
-// curl_close($ch);
-
-// echo $result;
 
