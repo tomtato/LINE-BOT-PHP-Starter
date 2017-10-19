1133f5de86a6df2ceee629160d9413b46e895f15
soraj <buasri_soraj@hotmail.com>
2017-10-19 13:46:18 +0700

    no message

--- botpush.php
+++ botpush.php
@@ -4,20 +4,29 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '75c03f392f6e53d662d6f5a8db9e421f']);
 
-$url = 'https://api.line.me/v2/bot/room/u2e0cc06232508ddd44e6a79eb0e764bb/member/'.$userId;
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->pushMessage('<to>', $textMessageBuilder);
 
-$headers = array('Authorization: Bearer ' . $access_token);
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+// $access_token = 'qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU=';
 
-echo $result;
+// $userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+
+// $url = 'https://api.line.me/v2/bot/room/u2e0cc06232508ddd44e6a79eb0e764bb/member/'.$userId;
+
+// $headers = array('Authorization: Bearer ' . $access_token);
+
+// $ch = curl_init($url);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// $result = curl_exec($ch);
+// curl_close($ch);
+
+// echo $result;
 
