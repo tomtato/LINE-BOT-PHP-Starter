84806ce2525e4c71f8ed2b39d0a987f81fe41d06
soraj <buasri_soraj@hotmail.com>
2017-10-20 09:46:43 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -2,43 +2,20 @@
 
 require "vendor/autoload.php";
 
-//Read request body
-$body = file_get_contents('php://input');
+$httpRequestBody = "events"; 
 
-$api_secret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 
-//Signature validation
-if (validateSignature(getallheaders(), $body, $api_secret)) {
+$hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
+$signature = base64_encode($hash);
 
-    //Use the response for your needs
-    $resposne = json_decode($body);
-    $public_id = $resposne->public_id;
+// Compare X-Line-Signature request header string and the signature
 
-} else {
-    die("Validation failed");
-}
-
-function validateSignature($headers, $upload_response, $api_secret)
-{
-
-    // $signed_payload = $upload_response . $headers["X-Cld-Timestamp"];
-
-    //Compute a HMAC with the SHA256 hash function, using the api secret as the key and the signed_payload string as the message
-    if (sha1($api_secret) === $headers['X-Line-Signature']) {
-        
-        //To prevent against timing attacks, we compare the expected signature to each of the received signatures.
-        // if ($headers["X-Cld-Timestamp"] <= strtotime('-2 hours')) {
-        //     //Signatures match, but older than 2 hours
-        //     return false;
-        // } else {
-        // 	//Signatures match, and timestamp 
-        //     return true;
-        // }
-        echo "true";
-        // return true;
-    } else {
-        //Signatures NOT match
-        echo "false";
-        // return false;
-    }
+if (sha1($signature) === $headers['X-Line-Signature']) {
+    //signatures match
+    echo 'true';
+} 
+else {
+	echo 'false';
+    //signatures NOT match
 }
