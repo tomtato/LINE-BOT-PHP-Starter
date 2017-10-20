b725b29f5b48abb6fc4927b556e7b40fb2bdb13f
soraj <buasri_soraj@hotmail.com>
2017-10-20 09:26:18 +0700

    no message

---
+++ webhooks.php
@@ -0,0 +1,11 @@
+<?php // callback.php
+
+require "vendor/autoload.php";
+
+$httpRequestBody = "events"; 
+
+$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+
+$hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
+$signature = base64_encode($hash);
+// Compare X-Line-Signature request header string and the signature
