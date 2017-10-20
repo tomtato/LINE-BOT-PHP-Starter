6f98f2210e830dc0fa2a9584333abfeba2422fe7
soraj <buasri_soraj@hotmail.com>
2017-10-20 13:00:04 +0700

    no message

--- vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/echo_bot.php
+++ vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/echo_bot.php
@@ -22,30 +22,186 @@ $channelAccessToken = '<your channel access token>';
 $channelSecret = '<your channel secret>';
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-    switch ($event['type']) {
-        case 'message':
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']
+//var_dump($client->parseEvents());
+//$_SESSION['userId']=$client->parseEvents()[0]['source']['userId'];
+/*
+{
+  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+  "type": "message",
+  "timestamp": 1462629479859,
+  "source": {
+    "type": "user",
+    "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+  },
+  "message": {
+    "id": "325708",
+    "type": "text",
+    "text": "Hello, world"
+  }
+}
+*/
+$userId     = $client->parseEvents()[0]['source']['userId'];
+$replyToken = $client->parseEvents()[0]['replyToken'];
+$timestamp  = $client->parseEvents()[0]['timestamp'];
+$message    = $client->parseEvents()[0]['message'];
+$messageid  = $client->parseEvents()[0]['message']['id'];
+$profil = $client->profil($userId);
+$pesan_datang = $message['text'];
+//pesan bergambar
+if($message['type']=='text')
+{
+    if($pesan_datang=='1')
+    {
+        
+        
+        $balas = array(
+                            'replyToken' => $replyToken,                                                        
+                            'messages' => array(
+                                array(
+                                        'type' => 'text',                   
+                                        'text' => 'Halo '.$profil->displayName.', Anda memilih menu 1,'
+                                    )
                             )
-                        )
-                    ));
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log("Unsupporeted event type: " . $event['type']);
-            break;
+                        );
+                
     }
-};
-echo $client ;
+    else
+    if($pesan_datang=='2')
+    {
+        $get_sub = array();
+        $aa =   array(
+                        'type' => 'image',                                  
+                        'originalContentUrl' => 'https://medantechno.com/line/images/bolt/1000.jpg',
+                        'previewImageUrl' => 'https://medantechno.com/line/images/bolt/240.jpg' 
+                        
+                    );
+        array_push($get_sub,$aa);   
+        $get_sub[] = array(
+                                    'type' => 'text',                                   
+                                    'text' => 'Halo '.$profil->displayName.', Anda memilih menu 2, harusnya gambar muncul.'
+                                );
+        
+        $balas = array(
+                    'replyToken'    => $replyToken,                                                     
+                    'messages'      => $get_sub
+                 ); 
+        /*
+        $alt = array(
+                            'replyToken' => $replyToken,                                                        
+                            'messages' => array(
+                                array(
+                                        'type' => 'text',                   
+                                        'text' => 'Anda memilih menu 2, harusnya gambar muncul.'
+                                    )
+                            )
+                        );
+        */
+        //$client->replyMessage($alt);
+    }
+    else
+    if($pesan_datang=='3')
+    {
+        
+        $balas = array(
+                            'replyToken' => $replyToken,                                                        
+                            'messages' => array(
+                                array(
+                                        'type' => 'text',                   
+                                        'text' => 'Fungsi PHP base64_encode medantechno.com :'. base64_encode("medantechno.com")
+                                    )
+                            )
+                        );
+                
+    }
+    else
+    if($pesan_datang=='4')
+    {
+        
+        $balas = array(
+                            'replyToken' => $replyToken,                                                        
+                            'messages' => array(
+                                array(
+                                        'type' => 'text',                   
+                                        'text' => 'Jam Server Saya : '. date('Y-m-d H:i:s')
+                                    )
+                            )
+                        );
+                
+    }
+    else
+    if($pesan_datang=='6')
+    {
+        
+        $balas = array(
+                            'replyToken' => $replyToken,                                                        
+                            'messages' => array(
+                                array(
+                                        'type' => 'location',                   
+                                        'title' => 'Lokasi Saya.. Klik Detail',                 
+                                        'address' => 'Medan',                   
+                                        'latitude' => '3.521892',                   
+                                        'longitude' => '98.623596' 
+                                    )
+                            )
+                        );
+                
+    }
+    else
+    if($pesan_datang=='7')
+    {
+        
+        $balas = array(
+                            'replyToken' => $replyToken,                                                        
+                            'messages' => array(
+                                array(
+                                        'type' => 'text',                   
+                                        'text' => 'Testing PUSH pesan ke anda'
+                                    )
+                            )
+                        );
+                        
+        $push = array(
+                            'to' => $userId,                                    
+                            'messages' => array(
+                                array(
+                                        'type' => 'text',                   
+                                        'text' => 'Pesan ini dari medantechno.com'
+                                    )
+                            )
+                        );
+                        
+        
+        $client->pushMessage($push);
+                
+    }
+    else{
+        $balas = array(
+                            'replyToken' => $replyToken,                                                        
+                            'messages' => array(
+                                array(
+                                        'type' => 'text',                   
+                                        'text' => 'Halo.. Selamat datang di medantechno.com .        Untuk testing menu pilih 1,2,3,4,5 ... atau stiker'
+                                    )
+                            )
+                        );
+                        
+    }
+}else if($message['type']=='sticker')
+{   
+    $balas = array(
+                            'replyToken' => $replyToken,                                                        
+                            'messages' => array(
+                                array(
+                                        'type' => 'text',                                   
+                                        'text' => 'Terimakasih stikernya... '                                       
+                                    
+                                    )
+                            )
+                        );
+                        
+}
+ 
+$result =  json_encode($balas);
+//$result = ob_get_clean();
+file_put_contents('./balasan.json',$result);
+$client->replyMessage($balas);
