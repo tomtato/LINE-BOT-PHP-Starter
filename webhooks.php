04069105fff913a53ed093c74471bcb545d90b4f
soraj <buasri_soraj@hotmail.com>
2017-10-20 10:16:03 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -2,7 +2,7 @@
 
 require "vendor/autoload.php";
 
-$httpRequestBody = "qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU="; 
+$channelAccessToken = "qFYA2+S09Lq3O63m8YkZQfS7Q5c3MUBgi70fvM7IMCvVMU1qvJ+UbVWJtHnonKAnCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRViXziTGtdULF4TXqNDretymx0c/7xoEW/jGKM2rJcQHQdB04t89/1O/w1cDnyilFU="; 
 
 $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 
@@ -12,29 +12,6 @@ $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 // // Compare X-Line-Signature request header string and the signature
 // echo $signature;
 
-$sig_check = 'sha1=' . hash_hmac('sha1', Request::getContent(), $_ENV[$channelSecret]);
-if ($sig_check === Request::header('X-Line-Signature')) {  // php >=5.6 and above should use hash_equals() for comparison
-  // sigs match, do stuff
-	echo 'true';
-}
 
-// $headers = getallheaders();
-// $hubSignature = $headers['X-Line-Signature'];
- 
-// // Split signature into algorithm and hash
-// list($algo, $hash) = explode('=', $hubSignature, 2);
- 
-// // Get payload
-// $payload = file_get_contents('php://input');
- 
-// // Calculate hash based on payload and the secret
-// $payloadHash = hash_hmac($algo, $payload, $channelSecret);
- 
-// // Check if hashes are equivalent
-// if ($hash !== $payloadHash) {
-//     // Kill the script or do something else here.
-//     die('Bad secret');
-// }
- 
-// // Your code here.
-// $data = json_decode($payload);
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+echo $client ;
