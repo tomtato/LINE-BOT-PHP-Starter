4d578bf8cb623f6048637053be472b11add60b88
soraj <buasri_soraj@hotmail.com>
2017-10-20 09:27:50 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -8,4 +8,6 @@ $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 
 $hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
 $signature = base64_encode($hash);
+
 // Compare X-Line-Signature request header string and the signature
+echo $signature ;
