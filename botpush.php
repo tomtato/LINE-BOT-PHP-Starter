65125e5b36f36cb5d289fb637e3ba076440c8d5f
soraj <buasri_soraj@hotmail.com>
2017-10-20 13:28:35 +0700

    no message

--- botpush.php
+++ botpush.php
@@ -11,8 +11,8 @@ $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('Uffa138efe037e6e889d0b0f4a871c005', $textMessageBuilder);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage('U7ef7a449f2a5c2057eacfc02ba2eb286', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
