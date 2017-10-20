41d1e3dda18cbe4fb3449f76202423941b57005d
soraj <buasri_soraj@hotmail.com>
2017-10-20 09:47:57 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -10,12 +10,12 @@ $hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
 $signature = base64_encode($hash);
 
 // Compare X-Line-Signature request header string and the signature
-
-if (sha1($signature) === $headers['X-Line-Signature']) {
-    //signatures match
-    echo 'true';
-} 
-else {
-	echo 'false';
-    //signatures NOT match
-}
+echo $signature;
+// if (sha1($signature) === $headers['X-Line-Signature']) {
+//     //signatures match
+//     echo 'true';
+// } 
+// else {
+// 	echo 'false';
+//     //signatures NOT match
+// }
