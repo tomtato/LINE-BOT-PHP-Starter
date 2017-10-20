0a39e05f3105d2be2ccd4329edd95eb676ea0192
soraj <buasri_soraj@hotmail.com>
2017-10-20 12:54:17 +0700

    no message

--- webhooks.php
+++ webhooks.php
@@ -13,27 +13,9 @@ $channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
 // // Compare X-Line-Signature request header string and the signature
 // echo $signature;
 
-// $client = new LINEBotTiny($channelAccessToken, $channelSecret);
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
-$header = array(
-            "Content-Type: application/json",
-            'Authorization: Bearer ' . $this->channelAccessToken,
-        );
 
-$context = stream_context_create(array(
-    "http" => array(
-        "method" => "POST",
-        "header" => implode("\r\n", $header),
-        "content" => json_encode("hello ja"),
-    ),
-));
-
-$response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
-if (strpos($http_response_header[0], '200') === false) {
-    http_response_code(500);
-    error_log("Request failed: " . $response);
-}
-var_dump($response );
 // echo  $client->sign("events") ;
 
 // $userId 	= $client->parseEvents()[0]['source']['userId'];
@@ -47,32 +29,32 @@ var_dump($response );
 // // echo "client : ".$client ;
 // print_r($client->replyMessage("testt"));
 // var_dump($client->parseEvents());
-// foreach ($client->parseEvents() as $event) {
-//     switch ($event['type']) {
-//         case 'message':
-//             $message = $event['message'];
-//             switch ($message['type']) {
-//                 case 'text':
-//                     $client->replyMessage(array(
-//                         'replyToken' => $event['replyToken'],
-//                         'messages' => array(
-//                             array(
-//                                 'type' => 'text',
-//                                 'text' => $message['text']
-//                             )
-//                         )
-//                     ));
-//                     echo "message : ".$client;
-//                     break;
-//                 default:
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    echo "message : ".$client;
+                    break;
+                default:
 
-//                     error_log("Unsupporeted message type: " . $message['type']);
-//                     break;
-//             }
-//             echo "Unsupporeted message type switch : ".$message['type'];
-//             break;
-//         default:
-//             error_log("Unsupporeted event type: " . $event['type']);
-//             break;
-//     }
-// };
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            echo "Unsupporeted message type switch : ".$message['type'];
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
