b7b4d5755dd8553e8363ed6d5f755d8a45c6bd47
tomtato <satang_1982@hotmail.com>
2019-07-16 06:31:47 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -12,7 +12,7 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-$response = $bot->pushMessage('U7ef7a449f2a5c2057eacfc02ba2eb286', $textMessageBuilder);
+$response = $bot->pushMessage('Udf3e802040b36afd3eb46205ecdbfb8b', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
